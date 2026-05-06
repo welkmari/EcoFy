95b83e477ea83d137bab12452999d994f7396f24
lumina-studio <welktech46@gmail.com>
2026-05-06 16:50:15 -0300

    fix: adding middleware to cookies and login and removing some unused files

--- src/lib/supabase/server.ts
+++ src/lib/supabase/server.ts
@@ -14,12 +14,12 @@ export async function createClient() {
         },
         setAll(cookiesToSet) {
           try {
-            cookiesToSet.forEach(({ name, value, options }) =>
-              cookieStore.set(name, value, options)
-            );
+            cookiesToSet.forEach(({ name, value, options }) => {
+              cookieStore.set(name, value, options);
+            });
           } catch {}
         },
       },
-    }
+    },
   );
 }
